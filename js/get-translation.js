c24f6318f7ec4b71188eca9640521fe799b70b5a
Daniel Lewis <daniellewis777@gmail.com>
2018-03-11 18:25:13 +0000

    tidied code

--- js/get-translation.js
+++ js/get-translation.js
@@ -1,36 +1,41 @@
 const {find, map, upperCase} = require('lodash')
 const lookup = require('./lookup')
-const pkg = require('../package.json')
+const {name} = require('../package.json')
 
 const getTranslation = q => {
 
-  if (typeof q !== 'string') throw TypeError(`${pkg.name} expects a string`)
-
-  const translations = lookup.getTranslations()
+  if (typeof q !== 'string') throw TypeError(`${name} expects a string`)
   q = q.toUpperCase()
 
+  const translations = lookup.getTranslations()
+
+  // find by abbr
   let translation = find(translations, {abbr: q})
 
+  // find by name
   if (!translation) {
     translation = find(translations, t => {
       return t.name.toUpperCase() === q
     })
   }
 
+  // find by alias
+  if (!translation) {
+    translation = find(translations, t => {
+      return map(t.aliases, upperCase).includes(q)
+    })
+  }
+
+  // find by gAbbr
   if (!translation) {
     translation = find(translations, {gAbbr: q})
   }
 
+  // find by gName
   if (!translation) {
     translation = find(translations, t => {
       if (t.gName == null) return false
       return t.gName.toUpperCase() === q
-    })
-  }
-
-  if (!translation) {
-    translation = find(translations, t => {
-      return map(t.aliases, upperCase).includes(q)
     })
   }
 
