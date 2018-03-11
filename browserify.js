eab0b31a3b9deba9dd93ca141081b433b2470739
Daniel Lewis <daniellewis777@gmail.com>
2018-03-11 02:41:29 +0000

    Updated browserify process

--- browserify.js
+++ browserify.js
@@ -1,6 +1,7 @@
-const webpackConfig = require('./webpack.config')
-
+const chalk = require('chalk')
 const exec = require('shelljs.exec')
+
+const webpackConfig = require('./webpack.config')
 const filename = webpackConfig.output.filename
 
 let cmdObj
@@ -12,7 +13,7 @@ cmdObj = exec(`git commit -m "Updated ${filename}" ${filename}`, {silent: true})
 count += cmdObj.code
 
 if (count === 0) {
-  console.log(`Committed ${filename}`)
+  console.log(chalk.green(`Committed ${filename}`))
 } else {
-  console.log(`Did not commit ${filename}`)
+  console.log(chalk.yellow(`Did not commit ${filename}`))
 }
